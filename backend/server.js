a90c4e2cfd0cc31302c51d293506e1ff0f24f112
loc-giang <locbgiang@gmail.com>
2024-01-01 18:46:52 -0800

    server set

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+const express = require("express") // import express
+const app = express()               // get the app portion from express
+const expressLayouts = require('express-ejs-layouts')   // get the express layout package
+
+const indexRouter = require('./routes/index')       // connects index.js
+
+// configuring
+app.set('view engine', 'ejs')       // set our view engine, using ejs
+app.set('views', __dirname + '/views')   // set where our views are coming from, current directory + views
+app.set('layout', 'layouts/layout') // every file is going to be put into this layout file
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+const mongoose = require('mongoose')    // import mongoose
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true
+})
+
+const db = mongoose.connection
+db.on('error', error => console.error(error))       // error message
+db.once('open', () => console.log('Connected to Mongoose'))     // connected message
+
+app.use('/', indexRouter)       // use index.js
+
+app.listen(process.env.PORT || 3000) // pull from environment, default to port 3000
