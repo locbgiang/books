9d6a86597f8cbcfa586dca5973565a266cfe6e9a
loc-giang <locbgiang@gmail.com>
2024-01-12 10:59:03 -0800

    initial author

--- backend/routes/authors.js
+++ backend/routes/authors.js
@@ -9,11 +9,11 @@ router.get('/', async (req, res) =>{
         searchOptions.name = new RegExp(req.query.name, 'i')
     }
     try {
-        const authors = await Author.find({searchOptions})
+        const authors = await Author.find(searchOptions)
         res.render('authors/index', {
             authors: authors,
             searchOptions: req.query
-        })
+        });
     } catch {
         res.redirect('/')
     }
