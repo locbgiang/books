350911dd516c7cfee7717d22eb8c6c6ca5bcc6f2
loc-giang <locbgiang@gmail.com>
2024-01-17 17:17:40 -0800

    books

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express') // import express
 const router = express.Router()     // grab router portion from express
+const Book = require('../models/book')
 
-router.get('/', (req, res)=>{
-    res.render('index')
+router.get('/', async (req, res)=>{
+    let books = []
+    try {
+        books = await Book.find().sort({ createAt: 'desc'}).limit(10).exec()
+    } catch {
+        books = []
+    }
+    res.render('index', { books: books })
 })
 
 module.exports = router
